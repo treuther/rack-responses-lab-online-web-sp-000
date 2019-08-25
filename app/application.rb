a8519774931a5fb3de92c25d4d033de6d230f6cc
Tracy Reuther <tracyreuther@gmail.com>
2019-08-25 18:11:03 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,4 @@
+# If it's before noon, greet the user with "Good Morning!"
+# If it's after noon, greet the user with "Good Afternoon!".
+# Remember that getting the time may return results in 24 hour time!
+# Anytime after 12 is the afternoon.
